8e6bdd97cd57220a2e0e05336b48476a794d562e
idopshik <idopshik@gmail.com>
2020-03-19 20:57:54 +0300

    "com port unawailible" handling added.

--- timer.js
+++ timer.js
@@ -1,27 +1,51 @@
-
-
-const SerialPort = require('serialport')
-const Readline = SerialPort.parsers.Readline
-const port = new SerialPort("/dev/ttyUSB0")
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort('/dev/ttyUSB0');
 
 var opn = require('opn');
-const parser = port.pipe(new Readline({ delimiter: '\n' }))
-parser.on('data', function(line){
+const parser = port.pipe(new Readline({delimiter: '\n'}));
 
-    let second
-    let min;
-    try {
-        second = line.match(/\d+/)[0];
-        min = second/60;
-        
-    } catch (err) {
-        console.log("WRONG DEVICE");
-    }
-    if (min > 3) console.log(` ${Math.floor(min)} min`);
-    else console.log(" < 3 min");
-    if(second < 3){
-        // opens the url in the default browser 
-        opn('https://this-page-intentionally-left-blank.org/');
-        return process.exit(22);
-    };
-})
+var delayedexit = false;
+function makedecision() {
+  console.log('well, continue');
+  if (delayedexit) {
+    console.log("can't reach device. Check com port availibility");
+    process.exit(22);
+  }
+}
+function delayhandler(a) {
+  if (a) {
+    delayedexit = false;
+    return;
+  } else if (!delayedexit) {
+    console.log('timer set. waiting for meaningfull input. ');
+    delayedexit = true;
+    setTimeout(makedecision, 500);
+  }
+}
+parser.on('data', function(line) {
+  let second;
+  let min;
+  let tmp = line.match(/\d+/);
+  let matchPhrase = line.match(/Second.to.count/);
+  if (!matchPhrase || !tmp) {
+    delayhandler();
+    return;
+  }
+  delayhandler(true);
+  if (delayedexit) return;
+
+  second = tmp[0];
+
+  min = second / 60;
+
+  if (min > 3) console.log(` ${Math.floor(min)} min`);
+  else console.log(' < 3 min');
+  if (second < 3 && second > 0) {
+    // opens the url in the default browser
+    opn('https://this-page-intentionally-left-blank.org/');
+    return process.exit(22);
+  } else if (!second) {
+    console.log('waiting for meaningfull input ...');
+  }
+});
