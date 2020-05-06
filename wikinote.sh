8fd71ca2def35515c2e05ee9688eb812c9c0111c
idopshik <idopshik@gmail.com>
2020-05-06 14:09:04 +0300

    custome vimrc_special

--- wikinote.sh
+++ wikinote.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-kitty vim -c "execute 'VimwikiMakeDiaryNote' | $ | put =strftime('%H:%M') | norm $ a  "
+kitty vim -u /home/st/Documents/MyJsScripts/github/littletimer/vimrc_special -c "execute 'VimwikiMakeDiaryNote' | $ | put =strftime('%H:%M') | norm $ a  "
